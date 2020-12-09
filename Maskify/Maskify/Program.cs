e96fcf4210d42a28ce2c4f097071a9276abe7606
sangithajk <43965922+sangithajk@users.noreply.github.com>
2020-12-09 23:24:56 +0530

    Maskify

---
+++ Maskify/Maskify/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+
+namespace Maskify
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Maskify("Hello World!"));
+        }
+        public static string Maskify(string cc)
+        {
+            StringBuilder newstr = new System.Text.StringBuilder();
+            for (int i = 0; i < cc.Length; i++)
+            {
+                if (i < cc.Length - 4)
+                    newstr.Append("#");
+
+                else
+                    newstr.Append(cc[i]);
+            }
+            return Convert.ToString(newstr);
+        }
+    }
+}
