4648ba2f2647a87dae216625ab6389fe323ab403
sangithajk <43965922+sangithajk@users.noreply.github.com>
2020-12-10 01:09:25 +0530

    Convert to Binary using Convert.ToString

---
+++ AddAndConvertToBinary/AddAndConvertToBinary/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AddAndConvertToBinary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(AddBinary(1,2));
+        }
+        public static string AddBinary(int a, int b)
+        {
+            // your code ...
+            // Convert.ToString -> 2nd parameter will define the format of output. 2 is for binary
+           
+
+            var sum = a + b;
+            return Convert.ToString(sum,2);
+        }
+    }
+}
