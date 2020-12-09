8564749b07a7afb09dfb19e05b3463d096f767b5
sangithajk <43965922+sangithajk@users.noreply.github.com>
2020-12-10 01:48:27 +0530

    Persistence using loops

---
+++ Persistence/Persistence/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Persistence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Persistence(66276));
+        }
+        public static long Persistence(long n)
+        {
+            // your code
+            int i = 10;
+            long result = 1;
+            int peristence = 0;
+            while (Convert.ToString(n).Length > 1)
+            {
+                result = 1;
+                peristence += 1;
+                while (n > 1)
+                {
+                    result *= (n % i);
+                    n /= i;
+                    
+                }
+                n = result;
+            }
+            Console.WriteLine("persistence : " + peristence);
+            return peristence;
+        }
+    }
+}
+        
+        
+        
