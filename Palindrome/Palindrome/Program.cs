0436be61edbddaada35ed7dea6217a21446cad4c
sangithajk <43965922+sangithajk@users.noreply.github.com>
2020-12-13 21:06:23 +0530

    Palindrome

---
+++ Palindrome/Palindrome/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Palindrome("ramamar"));
+            
+        }
+        public static bool Palindrome (string input)
+        {
+            var chararray = input.ToCharArray();
+            Array.Reverse(chararray);
+            if (input == new string(chararray))
+                return true;
+            else
+                return false;
+
+        }
+    }
+}
---
+++ Palindrome/Palindrome/SampleTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+
+namespace Palindrome
+{
+    [TestFixture]
+    public class SampleTest
+    {
+        [Test]
+        public  static void Sample_test1()
+        {
+            Console.WriteLine("Inside test");
+            Assert.IsTrue(Program.Palindrome("roadaor"));
+        }
+    }
+}
