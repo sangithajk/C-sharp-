32eee483e5ef33784dbae28c435af37fa08bbf44
sangithajk <43965922+sangithajk@users.noreply.github.com>
2020-12-13 23:29:09 +0530

    HttpClient Example

---
+++ HttpClientExample/HttpClientExample/Data.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HttpClientExample
+{
+   
+
+    public class Data
+    {
+        public int userId { get; set; }
+        public int id { get; set; }
+        public string title { get; set; }
+        public bool completed { get; set; }
+
+        public override string ToString()
+        {
+            return $"{userId},{id},{title},{completed}";
+        }
+
+    }
+
+}
---
+++ HttpClientExample/HttpClientExample/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+
+namespace HttpClientExample
+{
+    class Program
+    {
+        static async  Task  Main(string[] args)
+        {
+            //var HttpClient = HttpClientFactory.Create();
+            //var url = "https://jsonplaceholder.typicode.com/todos";
+            //var data = await HttpClient.GetStringAsync(url);
+            //Console.WriteLine(data);
+
+            var HttpClient = HttpClientFactory.Create();
+            var url = "https://jsonplaceholder.typicode.com/todos/1";
+            HttpResponseMessage HttpResponse = await HttpClient.GetAsync(url);
+            if(HttpResponse.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                var content = HttpResponse.Content;
+                //var data = await content.ReadAsStringAsync();
+                var data = await content.ReadAsAsync<Data>();
+                Console.WriteLine(data);
+            }
+        }
+    }
+}
